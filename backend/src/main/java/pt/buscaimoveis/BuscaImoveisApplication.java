4e1a395c066250d5077ff3fe1e6e0df874d08c63
fernando-arvelos <fernandoarvelos1@gmail.com>
2024-09-29 17:59:33 -0300

    Reestruturando as pastas

---
+++ backend/src/main/java/pt/buscaimoveis/BuscaImoveisApplication.java
@@ -0,0 +1,13 @@
+package pt.buscaimoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaImoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaImoveisApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pt/buscaimoveis/BuscaImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.buscaimoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaImoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

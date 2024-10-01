410cb9b431565086b6526a294e789c0bfcb68eb1
fernando-arvelos <fernandoarvelos1@gmail.com>
2024-10-01 18:12:50 -0300

    Resolvido algumas configurações de acesso as rotas

--- backend/src/main/java/pt/buscaimoveis/models/repositories/PropertyRepository.java
+++ backend/src/main/java/pt/buscaimoveis/models/repositories/PropertyRepository.java
@@ -4,6 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pt.buscaimoveis.models.entities.Property;
 
+import java.util.List;
+
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
+
+    List<Property> findByDistrito(String distrito);
+
+    List<Property> findByConcelho(String concelho);
+
+    List<Property> findByDistritoAndConcelho(String distrito, String concelho);
 }
--- backend/src/main/java/pt/buscaimoveis/security/SecurityConfig.java
+++ backend/src/main/java/pt/buscaimoveis/security/SecurityConfig.java
@@ -1,4 +1,33 @@
 package pt.buscaimoveis.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy((SessionCreationPolicy.STATELESS)))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+                                "/swagger-resources/**", "/webjars/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/properties").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/properties").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+
 }

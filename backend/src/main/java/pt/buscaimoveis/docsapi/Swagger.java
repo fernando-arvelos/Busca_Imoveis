7394660b981af4375725ebcc8a8d098aaf81fba8
fernando-arvelos <fernandoarvelos1@gmail.com>
2024-11-13 13:17:09 -0300

    Adicionar api key para segurança da API

--- backend/src/main/java/pt/buscaimoveis/docsapi/Swagger.java
+++ backend/src/main/java/pt/buscaimoveis/docsapi/Swagger.java
@@ -4,37 +4,22 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.ForwardedHeaderFilter;
 
-
-@SuppressWarnings("ALL")
 @Configuration
-public class Swagger implements OpenApiCustomizer {
+public class Swagger {
 
-  public final String schemeName = "Bearer Auth";
-
-  @Override
-  public void customise(OpenAPI openApi) {
-    Info info = new Info()
-        .title("Busca Imóveis API")
-        .description("""
-            Busca de imóveis em Portugal.
-            """)
-        .version("1.0.0");
-
-    openApi.info(info);
-
-    openApi.getComponents().addSecuritySchemes(schemeName,
-        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"));
-    openApi.addSecurityItem(new SecurityRequirement().addList(schemeName));
-  }
-
-  
   @Bean
-  ForwardedHeaderFilter forwardedHeaderFilter() {
-    return new ForwardedHeaderFilter();
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+            .info(new Info().title("Busca Imóveis API").version("1.0"))
+            .addSecurityItem(new SecurityRequirement().addList("ApiKeyAuth"))
+            .components(new io.swagger.v3.oas.models.Components()
+                    .addSecuritySchemes("ApiKeyAuth", new SecurityScheme()
+                            .type(SecurityScheme.Type.APIKEY)
+                            .in(SecurityScheme.In.HEADER)
+                            .name("x-api-key"))); // Define o nome do header como "x-api-key"
   }
 }
--- backend/src/main/java/pt/buscaimoveis/security/SecurityConfig.java
+++ backend/src/main/java/pt/buscaimoveis/security/SecurityConfig.java
@@ -2,32 +2,55 @@ package pt.buscaimoveis.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpMethod;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final ApiKeyAuthFilter apiKeyAuthFilter;
+
+    public SecurityConfig(ApiKeyAuthFilter apiKeyAuthFilter) {
+        this.apiKeyAuthFilter = apiKeyAuthFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(
-                        session -> session.sessionCreationPolicy((SessionCreationPolicy.STATELESS)))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
-                                "/swagger-resources/**", "/webjars/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/properties").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/properties").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().permitAll()
                 )
+                .addFilterBefore(apiKeyAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://buscaimoveis.pt", "https://buscaimoveis.pt"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*")); // Permite todos os headers
+        configuration.setExposedHeaders(List.of("x-api-key"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }

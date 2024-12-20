c7693e90fa081108d92225b8dff19b784cce3ada
fernando-arvelos <fernandoarvelos1@gmail.com>
2024-12-20 12:28:18 -0300

    Retirado API Key, pois estava ocasionando erros

--- backend/src/main/java/pt/buscaimoveis/docsapi/Swagger.java
+++ backend/src/main/java/pt/buscaimoveis/docsapi/Swagger.java
@@ -2,9 +2,7 @@ package pt.buscaimoveis.docsapi;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,11 +13,6 @@ public class Swagger {
   public OpenAPI customOpenAPI() {
     return new OpenAPI()
             .info(new Info().title("Busca Imóveis API").version("1.0"))
-            .addSecurityItem(new SecurityRequirement().addList("ApiKeyAuth"))
-            .components(new io.swagger.v3.oas.models.Components()
-                    .addSecuritySchemes("ApiKeyAuth", new SecurityScheme()
-                            .type(SecurityScheme.Type.APIKEY)
-                            .in(SecurityScheme.In.HEADER)
-                            .name("x-api-key"))); // Define o nome do header como "x-api-key"
+            .addServersItem(new Server().url("https://buscaimoveisapi.pt"));
   }
 }
--- backend/src/main/java/pt/buscaimoveis/security/SecurityConfig.java
+++ backend/src/main/java/pt/buscaimoveis/security/SecurityConfig.java
@@ -5,9 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,12 +17,6 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final ApiKeyAuthFilter apiKeyAuthFilter;
-
-    public SecurityConfig(ApiKeyAuthFilter apiKeyAuthFilter) {
-        this.apiKeyAuthFilter = apiKeyAuthFilter;
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -34,8 +26,6 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().permitAll()
                 )
-                .addFilterBefore(apiKeyAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                //.sessionManagement(session -> session.SessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 
@@ -49,7 +39,6 @@ public class SecurityConfig {
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*")); // Permite todos os headers
-        configuration.setExposedHeaders(List.of("x-api-key"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();

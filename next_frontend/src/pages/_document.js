afe8067ece4e38efcdda6cfc8fb7d0c032080e6b
Jenifer Miranda <jnf.asm@gmail.com>
2025-01-10 18:53:32 +0000

    atualiza tag do google no next

--- next_frontend/src/pages/_document.js
+++ next_frontend/src/pages/_document.js
@@ -6,14 +6,14 @@ export default function Document() {
       <Head>
         <link rel="icon" type="image/svg+xml" href="/vite.svg" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js? client=ca-pub-3822292795194891" crossOrigin="anonymous"></script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E6CK0STKW0"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TYQVJKWQ7P"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-E6CK0STKW0');
+              gtag('config', 'G-TYQVJKWQ7P');
             `,
           }}
         ></script>

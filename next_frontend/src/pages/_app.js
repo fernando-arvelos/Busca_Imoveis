c733eb364feb35e41c42ea7c05428aaf7906d989
LeonardoNunesTrybe <leonardonunes.uberbraga@gmail.com>
2025-01-16 22:02:31 +0000

    adicionando casinha junto ao title

--- next_frontend/src/pages/_app.js
+++ next_frontend/src/pages/_app.js
@@ -11,6 +11,7 @@ export default function MyApp({ Component, pageProps }) {
         <title>Busca Imóveis - Imóveis de bancos em Portugal com condições diferenciadas - Casas com 0 de Entrada | buscaimoveis.pt</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
         <meta name="description" content="Encontre imóveis de bancos em Portugal com 0 entrada e taxas reduzidas. Moradias, apartamentos, terrenos, entre outros à venda em Lisboa, Porto, Braga e mais regiões. Simples e confiável! Melhor buscador de imóveis de bancos em Portugal. Contamos com mais de 2.000 imóveis cadastrados com condições imperdíveis." />        
       </Head>
       <Header />
